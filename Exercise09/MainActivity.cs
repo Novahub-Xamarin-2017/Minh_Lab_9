7231aea78997e734936b1522e23283b6dbca4deb
U-DESKTOP-9OIDHC8\MSI <huynhngocminh2511@gmail.com>
2018-01-03 11:00:46 +0700

    Add Exercise09

---
+++ Exercise09/MainActivity.cs
@@ -0,0 +1,22 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Exercise09.CustomControls;
+
+namespace Exercise09
+{
+    [Activity(Label = "Exercise09", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            var bar = FindViewById<Bar>(Resource.Id.bar);
+        }
+    }
+}
+
