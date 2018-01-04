41888026bc6d1a52453b142496567c3d45032e9f
U-DESKTOP-9OIDHC8\MSI <huynhngocminh2511@gmail.com>
2018-01-04 13:36:18 +0700

    Add Exercise10

---
+++ Exercise10/CustomControls/Clock.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Graphics;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+
+namespace Exercise10.CustomControls
+{
+    public class Clock : View
+    {
+        public Clock(Context context, IAttributeSet attrs) :
+            this(context, attrs, 0)
+        {
+        }
+
+        public Clock(Context context, IAttributeSet attrs, int defStyle) :
+            base(context, attrs, defStyle)
+        {
+        }
+
+        public override void Draw(Canvas canvas)
+        {
+            base.Draw(canvas);
+
+        }
+    }
+}
